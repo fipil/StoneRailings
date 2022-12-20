2e1b825961dba72e1a793bdc7e3143ccaa9e9cd6
Filip Nowak <fipil@spincoders.com>
2022-12-20 21:58:31 +0100

    Updated modinfo

--- debug.ps1
+++ debug.ps1
@@ -10,18 +10,18 @@ $location = Get-Location
 # Preparing mod archive
 New-Item -ItemType Directory -Force -Path mods
 Remove-Item mods\*.zip
-Compress-Archive -Path assets,modinfo.json -DestinationPath mods\$zipname
+Compress-Archive -Path assets,modinfo.json,modicon.png -DestinationPath mods\$zipname
 
 # Running Vintage Story Server
 $arguments = "--openWorld=`"test$modname`" -pcreativebuilding --addModPath=`"$location\mods`" --dataPath $vsDataPath --tracelog"
-Start-Process -FilePath "$vsBinPath\Vintagestory.exe" -WorkingDirectory $vsBinPath -ArgumentList $arguments
+Start-Process -FilePath "$vsBinPath\VintagestoryServer.exe" -WorkingDirectory $vsBinPath -ArgumentList $arguments
 
 # Running Vintage Story Client
-# $title    = 'Client'
-# $question = 'Run Vintage Story client?'
-# $choices  = '&Yes', '&No'
-# $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
-# if ($decision -eq 0) {
-#     $arguments = "--addModPath=`"$location\mods`" --dataPath $vsDataPath"
-#     Start-Process -FilePath "$vsBinPath\Vintagestory.exe" -WorkingDirectory $vsBinPath -ArgumentList $arguments
-# }
+$title    = 'Client'
+$question = 'Run Vintage Story client?'
+$choices  = '&Yes', '&No'
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+    $arguments = "--addModPath=`"$location\mods`" --dataPath $vsDataPath"
+    Start-Process -FilePath "$vsBinPath\Vintagestory.exe" -WorkingDirectory $vsBinPath -ArgumentList $arguments
+}
